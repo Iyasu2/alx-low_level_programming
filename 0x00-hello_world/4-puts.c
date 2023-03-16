f9d4e2f5fdd1a8e9144a8256f10e6ac56ac6f235
Iyasu2 <iyasforreal@gmail.com>
2023-03-16 01:58:57 -0700

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printing using puts'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char *s = "\"Programming is like building a multilingual puzzle";
