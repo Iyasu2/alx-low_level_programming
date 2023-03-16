f5f9f0035217a847cde32e085d73c1196bd5d11c
Iyasu2 <iyasforreal@gmail.com>
2023-03-16 03:18:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printing using printf'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
