0b2a5dab6eb3ae6c8d932abbee200b4ede09ca5d
Iyasu2 <iyasforreal@gmail.com>
2023-03-16 01:27:46 -0700

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char *s = '"Programming is like building a multilingual puzzle';
+	char *s = "\"Programming is like building a multilingual puzzle";
 
 	puts(s);
 
