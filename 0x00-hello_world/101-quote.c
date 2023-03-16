68b961b68d8adf3b4d8db80e01f61b3abc0ded9c
iyasu2 <iyasuaworku@gmail.com>
2023-03-16 21:02:54 +0300

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description: 'printing sizes of various types'
+ *    * Parameters: no input returns an int
+ *     * Return: Always 0 (Success)
+ *     */
+
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main(void) 
+{ 
+	   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19!\n", 50); 
+	    
+	      return 0; 
+}
