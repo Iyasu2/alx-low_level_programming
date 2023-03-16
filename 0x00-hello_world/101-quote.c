44a2e3c7ccdb4bfa6c3743f1593f0facdca54755
iyasu2 <iyasuaworku@gmail.com>
2023-03-16 21:19:09 +0300

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   * Description: 'printing sizes of various types'
- *    * Parameters: no input returns an int
- *     * Return: Always 0 (Success)
- *     */
+ * write - write function
+ * Description: 'printing'
+ * Parameters: int filedes, const char *buf and unsigned in nbyte
+ * Return: Always 0 (Success)
+*/
 
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main(void) 
-{ 
-	   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19!\n", 50); 
-	    
-	      return 0; 
+int write(int filedes, const char *buf, unsigned int nbyte);
+
+/**
+ * main - Entry point
+ * Description: 'printing without printf'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
 }
