5ef71034145d9be6d9215215b27d2db6550e7fb0
Iyasu2 <iyasforreal@gmail.com>
2023-03-17 00:26:49 -0700

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'printing alphabets'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(i + 97);
+	}
+	putchar('\n');
+	return (0);
+}
