493d6e1a8ad504e20284b0cfa5757909f1928499
iyasu2 <iyasuaworku@gmail.com>
2023-03-17 17:26:53 +0300

    printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'printing numbers'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
