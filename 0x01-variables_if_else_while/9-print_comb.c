68075bed437538a6d58ed3a589bad3a53a110aa7
iyasu2 <iyasuaworku@gmail.com>
2023-03-17 19:28:42 +0300

    printcomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'printing alphabets'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
+
