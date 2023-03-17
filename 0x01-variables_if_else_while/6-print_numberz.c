36756523fc6b9043829af705818a8600ec791f50
iyasu2 <iyasuaworku@gmail.com>
2023-03-17 18:21:32 +0300

    printnumberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'printing alphabets'
+ * Parameters: no input returns an int
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
+}
