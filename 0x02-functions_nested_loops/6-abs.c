bc58cf92494c29b49f9ab67bf9d9806edcf7d6f6
iyasu2 <iyasuaworku@gmail.com>
2023-03-21 19:24:44 +0300

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,21 +5,11 @@
  * Parameters: input int n: 'number input to be converted'
  * Return: absolute value of n
  */
-int _abs(int)
+int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		_putchar('+');
-		return (1);
+		n = n * -1;
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	return n;
 }
