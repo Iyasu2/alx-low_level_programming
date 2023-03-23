7e6f14ac2d256e1880335653b068d9d314d25f09
Iyasu2 <iyasforreal@gmail.com>
2023-03-22 22:47:01 -0700

    printmostnumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ * Description: 'printing digits except 2 and 4 using putchar'
+ * Parameters: no input no return
+ * Return: none
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
