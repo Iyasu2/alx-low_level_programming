c79f6cd61131a49b39b4a90360696b3b5372c459
Iyasu2 <iyasforreal@gmail.com>
2023-03-22 23:54:49 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'fizz buzz test'
+ * Parameters: no input int return
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (((i + 1) % 3 == 0) && ((i + 1) % 5 != 0))
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (((i + 1) % 5 == 0) && ((i + 1) % 3 != 0))
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else if (((i + 1) % 5 == 0) && ((i + 1) % 3 == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		printf("%i ", i + 1);
+	}
+	printf("\n");
+	return 0;
+}
