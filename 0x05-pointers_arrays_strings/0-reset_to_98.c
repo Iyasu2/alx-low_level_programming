060ea542990cceb91bb4721629cab8f60e66ff1b
Iyasu2 <iyasforreal@gmail.com>
2023-03-27 03:52:09 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+void reset_to_98(int *n);
+/**
+ * reset_to_98 - Entry point
+ * Description: 'updating the value of an int using pointers'
+ * Parameters: int pointer input no return
+ * @*n: pointer to an integer n
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
