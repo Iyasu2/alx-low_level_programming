58592f9bd82d21a2b165e9c9eb252872578f9273
iyasu2 <iyasuaworku@gmail.com>
2023-03-29 20:33:45 +0300

    ss

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,14 +16,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] > s2[i])
-			{
-				n = 15;
-			}
-			else
-			{
-				n = -15;
-			}
+			n = s1[i] - s2[i];
 			break;
 		}
 	}
