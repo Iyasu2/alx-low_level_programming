9acd64530bddf665874370826c4b9181f1da923b
Iyasu2 <iyasforreal@gmail.com>
2023-04-03 00:42:30 -0700

    ss

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * Description: 'copying a string to another string'
+ * Parameters: two char and 1 unsigned int input returns a char
+ * @dest: char input 1
+ * @src: char input 2
+ * @n: unsigned int input
+ * Return: modified char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned i;
+	char *final;
+
+	for (i = 0; i < n && *dest != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	final = dest;
+
+	return (final);
+}
