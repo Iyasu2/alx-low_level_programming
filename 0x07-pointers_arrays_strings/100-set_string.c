d26079e402cdd779e91660c9d5ee26ec4497cfed
iyasu2 <iyasuaworku@gmail.com>
2023-04-03 22:35:12 +0300

    ss

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ * Description: 'value of a pointert to a char'
+ * Parameters: two char inputs no return
+ * @s: char input 1
+ * @to: char input 2
+ * Return: none
+ */
+void set_string(char **s, char *to)
+{
+	to = *s;
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,13 +11,13 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i = 0; *a != '\0'; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar((*a)[j]);
 			if (j == 7)
-				_putchar(' ');
+				_putchar('\n');
 		}
 		a++;
 	}
