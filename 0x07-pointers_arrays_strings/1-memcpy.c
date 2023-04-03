e436fc19c889e11ddef79f84ceb21970e603548e
iyasu2 <iyasuaworku@gmail.com>
2023-04-03 17:20:52 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned i;
+	unsigned int i;
 	char *final;
 
-	for (i = 0; i < n && *dest != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		*dest = *src;
 		dest++;
