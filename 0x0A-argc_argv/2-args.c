b366fa1db8a588604559b4b566cf34b073cfa384
iyasu2 <iyasuaworku@gmail.com>
2023-04-07 15:59:32 +0300

    args2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'printing all arguments'
+ * Parameters: int and a char input returns an int
+ * @argc: number of arguments
+ * @argv: array storing the arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
