cbf168377fa932b31e34a9c4a00ef20e76cb5390
iyasu2 <iyasuaworku@gmail.com>
2023-04-10 20:50:49 +0300

    allocgrid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _alloc_grid - Entry point
+ * Description: 'returning a 2d array'
+ *
+ * @width: int input 1
+ * @height: int input 2
+ * Return: an int
+ */
+int **alloc_grid(int width, int height)
+{
+	int k, i, j, h;
+	int **s;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	s = malloc(sizeof(int*) * width);
+	if (s == NULL)
+		return(NULL);
+	for (k = 0; k < width; k++)
+	{
+		s[k] = malloc(sizeof(int) * height);
+		if (s[k] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	return (s);
+	for (h = 0; h < width; h++)
+	{
+		free(s[h]);
+	}
+	free(s);
+}
