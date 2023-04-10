c02a272046ba9337b624062e83e2c7b8a6773333
iyasu2 <iyasuaworku@gmail.com>
2023-04-10 17:43:01 +0300

    strdup

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_arrray - Entry point
+ * Description: 'creating an array of chars'
+ * Parameters: int and char inputs returns a char
+ * @size: int input
+ * @c: char input
+ * Return: a char
+ */
+char *create_array(unsigned int size, char c)
+{
+
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Entry point
+ * Description: 'returning an allocated memory'
+ * Parameters: char input returns a char
+ * @str: char input
+ * Return: a char
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *s;
+	if (str == NULL)
+		return NULL;
+
+	s = malloc(sizeof(char) * 1000);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (s == NULL)
+		{
+			return NULL;
+		}
+		s[i] = str[i];
+	}
+	return (s);
+	free(s);
+}
+
