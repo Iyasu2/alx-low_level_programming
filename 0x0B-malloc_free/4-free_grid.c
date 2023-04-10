306969fd680d2c8e8d69278c8183d1283d3aace7
iyasu2 <iyasuaworku@gmail.com>
2023-04-10 21:56:51 +0300

    freegrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * free_grid - Entry point
+ * Description: 'freeing a 2d array'
+ *
+ * @grid: int input 1
+ * @height: int input 2
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
