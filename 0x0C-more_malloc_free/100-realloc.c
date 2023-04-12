0c053b00b925299fd20ae4c26af06af9211a8764
iyasu2 <iyasuaworku@gmail.com>
2023-04-12 20:16:38 +0300

    ss

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (s);
 	}
 	else if (new_size == old_size)
-		return ptr;
+		return (ptr);
 	else if (new_size > old_size)
 	{
 		s = malloc(new_size);
