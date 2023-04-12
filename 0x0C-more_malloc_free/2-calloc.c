05ec9cb41e8414fff97a8eee91be7afbc58f919d
iyasu2 <iyasuaworku@gmail.com>
2023-04-12 19:13:38 +0300

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Entry point
+ * Description: 'allocate memory'
+ *
+ * @nmemb: unsigned int input
+ * @size: unsigned int input
+ * Return: a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		s[i] = '0';
+	}
+
+	return (s);
+	free(s);
+}
