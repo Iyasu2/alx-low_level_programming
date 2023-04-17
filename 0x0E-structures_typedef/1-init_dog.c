a9f8dd0ccfdf3e41d7661dddcd76f712f9e750af
iyasu2 <iyasuaworku@gmail.com>
2023-04-17 14:06:38 +0300

    ss

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - Entry point
+ * Description: 'initialize a struct'
+ *
+ * @d: struct dog input
+ * @name: char input
+ * @age: float input
+ * @owner: char input
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = 
