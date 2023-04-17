aa4489d3115d1dcc67310417a575ec53dbb869bc
iyasu2 <iyasuaworku@gmail.com>
2023-04-17 15:02:32 +0300

    dogt

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: struct with 2 chars and a float
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
