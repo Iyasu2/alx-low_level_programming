f9a856874559a8e36553339765d4300cd5f0b031
iyasu2 <iyasuaworku@gmail.com>
2023-04-17 15:06:03 +0300

    ss

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
 /**
  * struct dog - struct with 3 types in it
  * @name: First member
@@ -9,14 +10,17 @@
  *
  * Description: struct with 2 chars and a float
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
+
