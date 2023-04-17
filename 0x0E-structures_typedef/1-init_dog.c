b87b87e6ac4800725631c4eaaf402849830cd813
iyasu2 <iyasuaworku@gmail.com>
2023-04-17 15:21:57 +0300

    ss

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * init_dog - Entry point
  * Description: 'initialize a struct'
@@ -11,7 +13,20 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (d == NULL)
+		return;
+	if (name != NULL)
+	{
+		d->name = malloc(strlen(name) + 1);
+		if (d->name != NULL)
+			d->name = name;
+	}
+
 	d->age = age;
-	d->owner = owner;
+	if (owner != NULL)
+	{
+		d->owner = malloc(strlen(name) + 1);
+		if (d->owner != NULL)
+			d->owner = owner;
+	}
 }
