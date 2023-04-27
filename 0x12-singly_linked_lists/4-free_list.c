8da2aba392a80c422732f0cf6d60a8f1227574db
iyasu2 <iyasuforreal@gmail.com>
2023-04-27 20:16:28 +0300

    freelist

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_list - Entry point
+ * Description: 'free linked lists'
+ *
+ * @head: list_t input
+ * Return: none
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
