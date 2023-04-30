b433d92b81485bd7c259276d96cad834ba75a2c7
iyasu2 <iyasuforreal@gmail.com>
2023-04-30 10:34:49 +0300

    insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * insert_nodeint_at_index - Entry point
+ * Description: 'add node at a given position of linked list'
+ *
+ * @head: list_t input
+ * @n: int input
+ * @idx: unsigned int input
+ * Return: adress of the new element or null
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new_node, *last_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	last_node = *head;
+	for (i = 0; i < idx; i++)
+	{
+		if (last_node == NULL)
+			return (NULL);
+		last_node = last_node->next;
+	}
+	new_node->next = last_node->next;
+	last_node = new_node;
+
+	return (new_node);
+}
