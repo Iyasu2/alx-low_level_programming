0777a37aabd4667a7a60313940a50c1b3157dab3
iyasu2 <iyasuforreal@gmail.com>
2023-04-30 09:44:44 +0300

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * pop_listint - Entry point
+ * Description: 'delete head of a linked lists'
+ *
+ * @head: listint_t input
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *temp;
+	if (*head == NULL)
+		return (0);
+
+	i = (*head)->n;
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+	return (i);
+}
