0a94c57f9138446a987b130e6402c78b5a726707
iyasu2 <iyasuforreal@gmail.com>
2023-05-01 12:16:23 +0300

    printsafe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint_safe - Entry point
+ * Description: 'printing all elements of list safely'
+ *
+ * @head: list_t input
+ * Return: size_t number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		nodes++;
+
+		if (head <= head->next)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			exit(98);
+		}
+		head = head->next;
+	}
+
+	return (nodes);
+}
