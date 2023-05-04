d0354946aa8e8a0c818adbb825958c260cecaf00
iyasu2 <iyasuforreal@gmail.com>
2023-05-04 19:54:37 +0300

    using bitmanu

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,39 +11,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int a = 0;
-	int i;
+	int i, n;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = strlen(b) - 1; i >= 0; i--)
+	for (i = 0, n = strlen(b); i < n; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		a += (pow(2, strlen(b) - 1 - i)) * (b[i] - '0');
+		a <<= 1;
+		if (b[i] == '1')
+			a += 1;
 	}
 	return (a);
 }
-/**
- * pow - Entry point
- * Description: 'calculate the power of'
- *
- * @a: double input
- * @b: double input
- * Return: double
- */
-double pow(double a, double b)
-{
-	double power = 1;
-	int i;
-
-	for (i = 0; i < b; i++)
-	{
-		power = power * a;
-	}
-	return (power);
-}
-
-
-
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,5 @@
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
-double pow(double a, double b);
 
 #endif /* MAIN_H */
