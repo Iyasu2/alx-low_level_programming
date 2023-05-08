4528aa89c064ac05014d72998dfa74d1abf1ddbe
iyasu2 <iyasuforreal@gmail.com>
2023-05-08 21:40:34 +0300

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_file(int fd);
+void copy_file(int fd_from, int fd_to);
+int open_file(char *file_name, int flags, mode_t mode);
+void usage(void);
 
 #endif /* MAIN_H */
