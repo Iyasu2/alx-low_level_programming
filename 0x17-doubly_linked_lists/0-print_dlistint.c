84b3beeb33d9a601bace55554f87a7762b89c7a9
iyasu2 <iyasuforreal@gmail.com>
2023-06-15 17:35:49 +0300

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - Entry point
+ * Description: 'printing all elements of doubly linked list'
+ *
+ * @h: dlistint_t input
+ * Return: size_t number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
