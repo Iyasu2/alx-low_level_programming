c95e8f368fad5342c7182659567222a913c512d0
Iyasu2 <iyasuforreal@gmail.com>
2023-06-19 10:48:15 +0300

    python and c

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: 'adding'
+ * @c: int 1
+ * @b: int 2
+ * Return: an int
+ */
+int add(int c, int b)
+{
+	return (c + b);
+}
+
+/**
+ * sub - Entry point
+ * Description: 'subtracting'
+ * @c: int 1
+ * @b: int 2
+ * Return: an int
+ */
+int sub(int c, int b)
+{
+	return (c - b);
+}
+
+/**
+ * mul - Entry point
+ * Description: 'multiplying'
+ * @c: int 1
+ * @b: int 2
+ * Return: an int
+ */
+int mul(int c, int b)
+{
+	return (c * b);
+}
+
+/**
+ * div - Entry point
+ * Description: 'dividing'
+ * @c: int 1
+ * @b: int 2
+ * Return: an int
+ */
+int div(int c, int b)
+{
+	return (c / b);
+}
+
+/**
+ * mod - Entry point
+ * Description: 'remainder'
+ * @c: int 1
+ * @b: int 2
+ * Return: an int
+ */
+int mod(int c, int b)
+{
+	return (c % b);
+}
